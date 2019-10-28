f6b91d94589260c6adc3e141366cd8983c6f179d
Jay Vaughan <ibisum@gmail.com>
2019-10-28 21:00:31 +0100

    Add filter to synth.lua

--- lua/synth.lua
+++ lua/synth.lua
@@ -4,8 +4,10 @@ local noteVelocity = synth:addParameter("_polyVelocity", 0.0);
 local voiceNumber = synth:addParameter("_polyVoiceNumber", 0.0);
 local pitchSine = synth:addParameter("pitchSine", 1.0);
 local pitchSquare = synth:addParameter("pitchSquare", 1.0);
-local mix = synth:addParameter("mix", 1.0)
-local modFreq = synth:addParameter("Modulator Frequency", 1.0);
+local mix = synth:addParameter("oscMix", 1.0)
+local modFreq = synth:addParameter("modFrequency", 1.0);
+local cutoffFreq = synth:addParameter("cutoffFrequency", 1.0);
+local cutoffQ = synth:addParameter("cutoffResonance", 1.0);
 
 local tone1 = SineWave()
 local tone2 = SquareWaveBL()
@@ -15,8 +17,12 @@ local voiceFreq = ControlMidiToFreq():input(noteNum)
 local attSine = (FixedValue(1.0) - (modOsc * mix)) / 2;
 local attSquare = (modOsc * mix) / 2;
 local tone = (tone1:freq(voiceFreq * pitchSine) * attSine) + (tone2:freq(voiceFreq * pitchSquare) * attSquare)
+
 local fv = FixedValue(0.02)
-   local env = ADSR()
+
+local lpf24F = LPF24():cutoff(FixedValue(2000) * cutoffFreq):Q(FixedValue(20) * cutoffQ)
+
+local env = ADSR()
        :attack(synth:addParameter("attack",0.1))
         :decay(synth:addParameter("decay", 0 ))
         :sustain(synth:addParameter("sustain",1))
@@ -24,4 +30,4 @@ local fv = FixedValue(0.02)
         :doesSustain(1)
         :trigger(gate);
 
-synth:setOutputGen((tone * env) * (fv + noteVelocity * 0.005));
+synth:setOutputGen((lpf24F:input(tone) * env) * (fv + noteVelocity * 0.005));
