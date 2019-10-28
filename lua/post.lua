ed6dfe4deffcb4d4d63f81377d8d6b56e0035231
kallaballa <amir@viel-zu.org>
2019-10-28 19:21:14 +0000

    tweaking patches

--- lua/post.lua
+++ lua/post.lua
@@ -2,12 +2,14 @@ local noteNum = synth:addParameter("_polyNote", 0.0);
 local gate = synth:addParameter("_polyGate", 0.0);
 local noteVelocity = synth:addParameter("_polyVelocity", 0.0);
 local voiceNumber = synth:addParameter("_polyVoiceNumber", 0.0);
+
+local gain = synth:addParameter("gain", 1.0)
 local vibratoAmount = synth:addParameter("vibratoAmount", 1.0);
 local vibratoFreq = synth:addParameter("vibratoFreq", 1.0);
 local tremoloAmount =synth:addParameter("tremoloAmount", 1.0);
 local tremoloFreq = synth:addParameter("tremoloFreq", 1.0);
 
-local vibrato = SineWave():freq(vibratoFreq * 10.0);
+local vibrato = SineWave():freq(vibratoFreq * 30.0);
 local tremolo = SineWave():freq(tremoloFreq * 10.0);
 
 local voiceFreq = ControlMidiToFreq():input(noteNum)
@@ -20,4 +22,4 @@ local env = ADSR()
 :doesSustain(1)
 :trigger(gate);
 
-synth:setOutputGen((tone * env * ( FixedValue(1.0) + (tremolo * (tremoloAmount * 3)) )) * (FixedValue(0.02) + noteVelocity * 0.005))
+synth:setOutputGen((tone * gain * env * ( FixedValue(1.0) + (tremolo * (tremoloAmount * 3)) )) * (FixedValue(0.02) + noteVelocity * 0.005))
