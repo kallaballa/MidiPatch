aaaae4225fa41c97bc53093ac752f7693e82ae1a
kallaballa <amir@viel-zu.org>
2019-10-29 18:52:35 +0100

    improve lcd class

--- src/LCD.cpp
+++ src/LCD.cpp
@@ -8,7 +8,7 @@
 #include "LCD.hpp"
 
 LCD& LCD::print(uint8_t row, uint8_t col, const std::string& s) {
-	size_t len = std::min((size_t)20, s.size() - col);
+	size_t len = std::min((size_t)20, s.size() + col);
 	memcpy((void *)(buffers_[row] + col), (void *)s.c_str(), len);
 	ttyOut_ << row;
 	ttyOut_.write(buffers_[row], 20);
@@ -17,7 +17,7 @@ LCD& LCD::print(uint8_t row, uint8_t col, const std::string& s) {
 }
 
 LCD& LCD::clear() {
-	for(uint8_t i = 0; i > 4; ++i) {
+	for(uint8_t i = 0; i < 4; ++i) {
 		memset((void *)(buffers_[i]), 0, 20);
 		ttyOut_ << i;
 		ttyOut_.write(buffers_[i], 20);
