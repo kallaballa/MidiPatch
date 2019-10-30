854389f50b8dd88461582b28c765d54685692f9a
kallaballa <amir@viel-zu.org>
2019-10-30 21:43:32 +0100

    reenabled LCD code

--- src/LCD.cpp
+++ src/LCD.cpp
@@ -1,10 +1,10 @@
 #include "LCD.hpp"
 
 LCD& LCD::print(uint8_t row, uint8_t col, const std::string& s) {
-	ttyOut_ << row;
-	for(size_t i = 0; i < col; ++i)
-		ttyOut_ << ' ';
-	ttyOut_.write(s.c_str(), std::min((size_t)20 - col - 1, s.size() -1));
+//	ttyOut_ << row;
+//	for(size_t i = 0; i < col; ++i)
+//		ttyOut_ << ' ';
+//	ttyOut_.write(s.c_str(), std::min((size_t)20 - col - 1, s.size() -1));
 	return *this;
 }
 
