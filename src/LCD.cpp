38d1215cb669d6e289a7dfc5d95961d365b5b603
kallaballa <amir@viel-zu.org>
2019-10-30 19:15:41 +0100

    reenabled LCD code

--- src/LCD.cpp
+++ src/LCD.cpp
@@ -1,13 +1,10 @@
 #include "LCD.hpp"
-#include <thread>
-#include <chrono>
 
 LCD& LCD::print(uint8_t row, uint8_t col, const std::string& s) {
 	ttyOut_ << row;
 	for(size_t i = 0; i < col; ++i)
 		ttyOut_ << ' ';
 	ttyOut_.write(s.c_str(), 20 - col);
-	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 	return *this;
 }
 
