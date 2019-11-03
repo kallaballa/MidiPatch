039c0f626ac74aea2759a0cd720ad18e266df482
kallaballa <amir@viel-zu.org>
2019-11-03 13:25:26 +0000

    improved run script

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/home/elchaschab/devel/Farts/third/Tonic/src/:/home/elchaschab/devel/Farts/third/lua-5.3.5/src/:/home/elchaschab/devel/Farts/third/RtMidi:/home/elchaschab/devel/Farts/third/RtAudio"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:third/Tonic/src/:/third/lua-5.3.5/src/:third/RtMidi:third/RtAudio"
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$DIR"
 ec=42
 while [ $ec -eq 42 ]; do
 	src/farts $@;
 	ec=$?;
-	echo "error code: $ec" 
 done
 
