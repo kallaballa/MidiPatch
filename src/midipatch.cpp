f91ca5e95a887333c486a4d5c6647c32318475f8
kallaballa <amir@viel-zu.org>
2019-11-10 10:27:55 +0100

    move synth allocation and all kaguya code into patchscript

--- src/midipatch.cpp
+++ src/midipatch.cpp
@@ -8,31 +8,27 @@
 #include <string.h>
 #include <mutex>
 
-#include "Tonic.h"
-#include "kaguya/kaguya.hpp"
 #include "cxxopts.hpp"
 #include "RtAudio.h"
-
-#include "logger.hpp"
-#include "tonic_lua.hpp"
 #include "RtAudio.h"
 #include "RtMidi.h"
 #include "RtError.h"
-#include "PolySynth.hpp"
-#include "Websocket.hpp"
+#include "lua.h"
 
-std::mutex midiMutex;
+#include "logger.hpp"
+#include "Websocket.hpp"
+#include "PatchScript.hpp"
+
 using namespace Tonic;
 
 const unsigned int nChannels = 2;
-
-Synth* synth;
-PolySynth* poly;
-
 uint8_t current_program = 127;
 size_t controlNumberOffset = 0;
-midipatch::Websocket* websocket = nullptr;
 string save_file;
+
+std::mutex midiMutex;
+patchscript::PatchScript* pscript = nullptr;
+midipatch::Websocket* websocket = nullptr;
 
 void log_error(const string& title, const string& msg = "") {
 	LOG_ERR_MSG(title, msg);
@@ -68,7 +64,7 @@ int renderCallback(void *outputBuffer, void *inputBuffer, unsigned int nBufferFr
 		RtAudioStreamStatus status, void *userData) {
 	if (websocket->isRestartRequested())
 		return 0;
-	synth->fillBufferOfFloats((float*) outputBuffer, nBufferFrames, nChannels);
+	pscript->fill((float*) outputBuffer, nBufferFrames, nChannels);
 	size_t lenBuf = nBufferFrames * nChannels;
 	std::vector<float> samples(lenBuf, 0);
 
@@ -93,24 +89,24 @@ void midiCallback(double deltatime, vector<unsigned char>* msg, void* userData) 
 
 	if (msgtype == 0x80 || (msgtype == 0x90 && b2 == 0)) {
 		std::cout << "MIDI Note OFF  C: " << chan << " N: " << b1 << std::endl;
-		poly->noteOff(b1);
+		pscript->getPolySynth()->noteOff(b1);
 		websocket->sendNoteOff(b1);
 	} else if (msgtype == 0x90) {
 		std::cout << "MIDI Note ON   C: " << chan << " N: " << b1 << " V: " << b2 << std::endl;
-		poly->noteOn(b1, b2);
+		pscript->getPolySynth()->noteOn(b1, b2);
 		websocket->sendNoteOn(b1, b2);
 	} else if (msgtype == 0xB0) {
 		std::cout << "MIDI CC ON     C: " << chan << " N: " << b1 << " V: " << b2 << std::endl;
 		std::vector<string> commonParams;
 
 		//try to set a common parameter for all synths. NOTE: only works if all synthesizers have the same public parameters
-		if (!poly->getVoices().empty()) {
+		if (!pscript->getPolySynth()->getVoices().empty()) {
 			Synth s;
 			std::vector<string> currentParams;
 			bool first = true;
 
 			//check if all parameters match between all synths
-			for (auto& pv : poly->getVoices()) {
+			for (auto& pv : pscript->getPolySynth()->getVoices()) {
 				currentParams.clear();
 				s = pv.synth;
 				for (auto& cp : s.getParameters()) {
@@ -134,7 +130,7 @@ void midiCallback(double deltatime, vector<unsigned char>* msg, void* userData) 
 			std::vector<string> publicParameters = currentParams;
 			string parent;
 			string child;
-			for (auto& pv : poly->getVoices()) {
+			for (auto& pv : pscript->getPolySynth()->getVoices()) {
 				s = pv.synth;
 
 				if ((b1 - controlNumberOffset) < publicParameters.size()) {
@@ -163,7 +159,7 @@ void midiCallback(double deltatime, vector<unsigned char>* msg, void* userData) 
 }
 
 void save_parameters() {
-	auto params = poly->getVoices()[0].synth.getParameters();
+	auto params = pscript->getPolySynth()->getVoices()[0].synth.getParameters();
 	ofstream ofs(save_file);
 	for (auto& p : params) {
 		ofs << p.getName() << (char) 0 << std::to_string(p.getValue()) << (char) 0;
@@ -180,7 +176,7 @@ void load_parameters() {
 		loadMap[string(buf0)] = std::stof(string(buf1));
 	}
 
-	for (auto& v : poly->getVoices()) {
+	for (auto& v : pscript->getPolySynth()->getVoices()) {
 		auto params = v.synth.getParameters();
 
 		for (auto& p : params) {
@@ -237,10 +233,10 @@ int main(int argc, char ** argv) {
 	std::ofstream ofLog(logFile);
 	std::cout.rdbuf(ofLog.rdbuf());
 	std::cerr.rdbuf(ofLog.rdbuf());
+	pscript = new patchscript::PatchScript(sampleRate);
 	websocket = new midipatch::Websocket(port, patchFile);
 
-	kaguya::State state;
-	state.setErrorHandler([](int status, const char* msg) {
+	pscript->setErrorHandler([](int status, const char* msg) {
 		switch (status) {
 			case LUA_ERRSYNTAX:
 				log_syntax_error("Syntax error", msg);
@@ -266,13 +262,9 @@ int main(int argc, char ** argv) {
 			break;
 		}
 	});
-	bindings0(state);
-	bindings1(state);
-	bindings2(state);
+
 
 	// You don't necessarily have to do this - it will default to 44100 if not set.
-	Tonic::setSampleRate(sampleRate);
-	std::vector<Synth*> s(numVoices, nullptr);
 	std::vector<RtMidiIn*> midiIn(midiIndex.size(),nullptr);
 
 	while (true) {
@@ -281,23 +273,11 @@ int main(int argc, char ** argv) {
 		rtParams.deviceId = audioIndex;
 		rtParams.nChannels = nChannels;
 
-		synth = new Synth();
-		poly = new PolySynth();
 		while (!websocket->hasClients()) {
 			sleep(1);
 		}
 		try {
-			for (size_t i = 0; i < numVoices; ++i) {
-				if (s[i] != nullptr) {
-					delete (s[i]);
-				}
-				s[i] = new Synth();
-				state["synth"] = s[i];
-				if (!state.dofile(patchFile)) {
-					break;
-				}
-				poly->addVoice(*s[i]);
-			}
+			bool initSuccess = pscript->init(patchFile, numVoices);
 			websocket->setSendConfigCallback([&]() {
 				std::ostringstream ss;
 				ss << "{ \"type\": \"config\", \"data\": { ";
@@ -310,38 +290,34 @@ int main(int argc, char ** argv) {
 			});
 			websocket->sendConfig();
 
-			if (!poly->getVoices().empty()) {
+			if (initSuccess) {
 				load_parameters();
 				signal(SIGINT, signalHandler);
 				signal(SIGTERM, signalHandler);
 				signal(SIGKILL, signalHandler);
 
-				//add a slight ADSR to prevent clicking
-				synth->setOutputGen(*poly);
-				// open rtaudio stream and rtmidi port
-
 				websocket->setSetControlCallback([&](string n, float v) {
-					for(auto& voice : poly->getVoices()) {
+					for(auto& voice : pscript->getPolySynth()->getVoices()) {
 						voice.synth.setParameter(n, v);
 					}
 				});
 				websocket->setNoteOnCallback([&](size_t note, size_t velo) {
-					poly->noteOn(note, velo);
+					pscript->getPolySynth()->noteOn(note, velo);
 				});
 
 				websocket->setNoteOffCallback([&](size_t note) {
-					poly->noteOff(note);
+					pscript->getPolySynth()->noteOff(note);
 				});
 
 				websocket->setClearAllNotesCallback([&]() {
-					poly->clearAllNotes();
+					pscript->getPolySynth()->clearAllNotes();
 				});
 				websocket->setSendControlListCallback([&]() {
 					std::ostringstream ss;
 					ss << "{ \"type\": \"control-list\", \"data\": [ ";
-					if(poly->getVoices().empty())
+					if(pscript->getPolySynth()->getVoices().empty())
 					return string("");
-					auto params = poly->getVoices()[0].synth.getParameters();
+					auto params = pscript->getPolySynth()->getVoices()[0].synth.getParameters();
 					std::vector<string> parents;
 					std::map<string, std::vector<std::pair<string, float>>> hierachie;
 					for (size_t i = 0; i < params.size(); ++i) {
@@ -437,8 +413,7 @@ int main(int argc, char ** argv) {
 					log_warn("Can't clean up audio port");
 				}
 				save_parameters();
-				delete (synth);
-				delete (poly);
+				pscript->destroy();
 			} else {
 				websocket->reset();
 				while (!websocket->isRestartRequested()) {
