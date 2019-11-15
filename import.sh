6c6e4f978e1c6fe91d54c3fea6a44141c846ee65
kallaballa <amir@viel-zu.org>
2019-11-15 17:11:13 +0100

    create import script

---
+++ import.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$OSTYPE" == "linux-gnu" -o "$OSTYPE" == "linux" ]; then
+	export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:PatchScript/src/:PatchScript/third/Tonic/src/:PatchScript/third/lua-5.3.5/src/:third/RtMidi:third/RtAudio"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	export DYLD_LIBRARY_PATH=third/RtAudio:third/RtMidi:PatchScript/third/Tonic/src:PatchScript/src/
+fi
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$DIR"
+PatchScript/src/fsimporter/fsimporter $@ 
+
+
