2f2a79a432806544f224ea455646841ab9bfc531
kallaballa <amir@viel-zu.org>
2019-11-17 17:50:31 +0100

    scroll active knobs into view

--- html/js/main.js
+++ html/js/main.js
@@ -620,6 +620,7 @@ function connect() {
               if(!isNaN(parseInt($(this).val(), 10))) {
                 setControl(par.parent().find("label").html() + "." + par.find("label").html(), $(this).val());
                 localStorage.setItem("savedControlParameters",JSON.stringify(getControlParameters()));
+                $(this).scrollIntoView();
               }
             });
             for (var i = 0; i < knobs.length; ++i) {
@@ -633,6 +634,7 @@ function connect() {
                         var par = this.$.parent().parent()
                         setControl(par.parent().find("label").html() + "." + par.find("label").html(), value);
                         localStorage.setItem("savedControlParameters",JSON.stringify(getControlParameters()));
+                        $(this).scrollIntoView();
                     }
                 });
             }
