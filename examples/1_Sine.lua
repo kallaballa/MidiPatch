3d1512b5c64b8b3f7ae00350841134f638bbe062
kallaballa <amir@viel-zu.org>
2019-11-18 20:41:44 +0100

    New examples

---
+++ examples/1_Sine.lua
@@ -0,0 +1,68 @@
+--[[
+This examples shows how to generate a sine wave from MIDI 
+events.
+]]--
+
+--[[
+The MIDI note parameters send by the engine. Note that they
+all start with an underscore, which means they are private 
+and are not included in the rack.
+]]--
+local noteNum = synth:addParameter("_polyNote", 0.0)
+local gate = synth:addParameter("_polyGate", 0.0)
+local noteVelocity = synth:addParameter("_polyVelocity", 0.0)
+local voiceNumber = synth:addParameter("_polyVoiceNumber", 0.0)
+
+--[[
+The global volume parameter. Scales the signal at the very end
+]]--
+local volume = synth:addParameter("Global.Volume", 0.3)
+
+--[[
+A ControlGenerator that takes MIDI note numbers as input and
+outputs the frequency in Hz.
+]]--
+local freq = ControlMidiToFreq():input(noteNum)
+
+
+--[[
+The sine wave oscillator generating the actual tone at the
+note frequency
+]]--
+local tone = SineWave():freq(freq);
+
+
+--[[
+This parameters is driven by the note velocity and leads to 
+a louder tone with a higher velocity.
+]]--
+local velocityMod = FixedValue(0.02) + noteVelocity * 0.005;
+
+--[[
+we need the envelope for two reasons:
+1. to prevent clicking on noteOn and noteOff (hence the slight
+   attack and release)
+2. So we can use it as a gate. Without it the oscillator would 
+   be generating a continous signal
+]]--
+local envelope = ADSR()
+	:attack(0.01)
+	:decay(0)
+	:sustain(1)
+	:release(0.01)
+	:doesSustain(1)
+	:trigger(gate)
+
+
+--[[
+Attenuate the "tone" (sine wave) by the "velocityMod" (higher 
+velocity -> louder tone). Then apply the "envelope" (which in
+our case does nearly nothing) and scale the signal by
+"volume". 
+]]--
+local signal = tone * velocityMod * envelope * volume;
+
+--[[
+Assign the generator "signal" to the synth.
+]]--
+synth:setOutputGen(signal);
---
+++ examples/3_OscAdder.lua
@@ -0,0 +1,68 @@
+--[[
+This example adds a number of oscillators with each oscillator
+having a slightly different pitch. It demonstrates how through
+scripting synthesizers can be created that are hard to create
+with traditional paradigmes.
+]]--
+
+local NUM_OSCILLATORS=10
+
+local noteNum = synth:addParameter("_polyNote", 0.0)
+local gate = synth:addParameter("_polyGate", 0.0)
+local noteVelocity = synth:addParameter("_polyVelocity", 0.0)
+local voiceNumber = synth:addParameter("_polyVoiceNumber", 0.0)
+
+local volume = synth:addParameter("Global.Volume", 0.3)
+local attack = synth:addParameter("Global.Attack",0.1)
+local decay = synth:addParameter("Global.Decay", 0 )
+local sustain = synth:addParameter("Global.Sustain",1)
+local release = synth:addParameter("Global.Release",0.1)
+local shiftAmount = synth:addParameter("Global.PitchShiftAmount", 1)
+
+local lpCutoff = synth:addParameter("Filter.LP_Freq", 1.0)
+local lpQ = synth:addParameter("Filter.LP_Q", 0.0)
+local hpCutoff = synth:addParameter("Filter.HP_Freq", 0.0)
+local hpQ = synth:addParameter("Filter.HP_Q", 0.0)
+
+local voiceFreq = ControlMidiToFreq():input(noteNum)
+
+function makeOSC(index, total, voiceFreq)
+  local name = "Oscillator" .. (index - 1)
+  local pitch = 1.0 - (0.01 * (index / total))
+  local gain = synth:addParameter(name .. ".Gain", 1.0)
+  local sine = synth:addParameter(name .. ".Sine", 1.0)
+  local sineP = synth:addParameter(name .. ".SinePitch", pitch)
+  local square = synth:addParameter(name .. ".Square", 0.25)
+  local squareP = synth:addParameter(name .. ".SquarePitch", pitch)
+  local triangle = synth:addParameter(name .. ".Triangle", 0.5)
+  local triangleP = synth:addParameter(name .. ".TrianglePitch", pitch)
+  return  (
+            (SineWave():freq(voiceFreq * (sineP * shiftAmount)) * sine) +
+            (SquareWave():freq(voiceFreq * (squareP * shiftAmount)) * square) +
+            (TriangleWave():freq(voiceFreq * (triangleP * shiftAmount)) * triangle)
+          ) * 0.33 * gain
+end
+
+local mix = Adder();
+for i=1,NUM_OSCILLATORS do
+	mix = mix + makeOSC(i,10,voiceFreq)     
+end
+
+local lpf = LPF24()
+	:cutoff(FixedValue(10000) * lpCutoff)
+	:Q(FixedValue(20) * lpQ)
+
+local hpf = HPF24()
+	:cutoff(FixedValue(10000) * hpCutoff)
+	:Q(FixedValue(20) * hpQ)
+
+local env = ADSR()
+	:attack(attack)
+	:decay(decay)
+	:sustain(sustain)
+	:release(release)
+	:doesSustain(1)
+	:trigger(gate)
+
+synth:setOutputGen(((hpf:input(lpf:input(mix)) * env)
+  * (FixedValue(0.02) + noteVelocity * 0.005)) * volume);
