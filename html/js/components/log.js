4e2f8c1e629048d464bf5f937de9ff3b9960de36
kallaballa <amir@viel-zu.org>
2019-11-21 11:40:23 +0100

    modularized log and toolbar

---
+++ html/js/components/log.js
@@ -0,0 +1,43 @@
+class MPLog {
+    constructor(editor) {
+        this.editor = editor;
+        this.logLevels = [ "Fatal", "Error", "Warning", "Info", "Debug", "Global" ];
+    }
+    
+    makeHTML() {
+        return '<div id="log"></div>'
+    }
+    
+    update(obj) {
+        var current_datetime = new Date();
+        var formatted_date = current_datetime.getFullYear() + "-" + appendLeadingZeroes(current_datetime.getMonth() + 1) + "-" + appendLeadingZeroes(current_datetime.getDate()) + " " + appendLeadingZeroes(current_datetime.getHours()) + ":" + appendLeadingZeroes(current_datetime.getMinutes()) + ":" + appendLeadingZeroes(current_datetime.getSeconds());
+        var color = "#ffffff";
+        if(obj.severity < 2)
+        color = "#ff0000";
+        else if(obj.severity < 3)
+        color = "#ffff00";
+        $("#log").append("<pre style=\"color: " + color +";\">" + formatted_date  + " ["+ this.logLevels[obj.severity] + "] " + obj.title + "</pre><br/>");
+        if(obj.msg.length > 0) {
+            if(!obj.highlight) {
+            $("#log").append("<pre style=\"color: " + color +";\">\t" + obj.msg + "</pre><br/>");
+            } else {
+                var tokens = obj.msg.split(":");
+                var file = tokens[0];
+                var lineNum = parseInt(tokens[1], 10) - 1;
+                var rest = "";
+                for(var i = 2; i < tokens.length; ++i) {
+                    rest += tokens[i];
+                    if(i < tokens.length - 1)
+                    rest += ":";
+                }
+                $("#log").append("<pre style=\"color: " + color +";\">\t<span>" + file + ":" + (lineNum + 1) + ":" + rest + "</span></pre>").last().click(function() {
+                    editor.focusLine(lineNum);
+                });
+                $("#log").append("<br/>");
+                $("#log").parent().attr("id","logparent");
+                var element = document.getElementById("logparent");
+                element.scrollTop = element.scrollHeight - element.clientHeight;
+            }
+        }
+    }
+}
---
+++ html/js/components/toolbar.js
@@ -0,0 +1,39 @@
+class MPToolbar {
+        constructor(midipatch) {
+            this.midipatch = midipatch;
+        }
+        
+        makeHTML() {
+            return '<div id="toolbar"><button class="ui-button ui-corner-all ui-widget" id="restart">Restart</button><button class="ui-button ui-corner-all ui-widget" id="loadpatch">Download</button><button class="ui-button ui-corner-all ui-widget" id="storepatch">Upload</button><button class="ui-button ui-corner-all ui-widget" id="exporttolibrarybtn">Export to library</button><button class="ui-button ui-corner-all ui-widget" id="resetlayout">Reset Layout</button><section id="editorselectsection"><label class="ui-corner-all ui-widget" id="editorselectlbl" for="editorselect">Mode:</label><select id="editorselect"><option value="sublime">Sublime</option><option value="vim">Vim</option><option value="emacs">Emacs</option></select></section><input type="checkbox" id="restartonsave" value="Restart on save" checked/><label for="restartonsave" id="restartonsavelbl">Restart on store</label><section id="connectionstatus">Status:<span>Disconnected</span></div></div>'
+        }
+        
+        bind() {
+            $("#restart").click(function() {
+                midipatch.restart();
+            });
+
+            $("#storepatch").click(function() {
+                midipatch.storePatch();
+            });
+
+            $("#loadpatch").click(function() {
+                midipatch.loadPatchAsk();
+            });
+
+            $("#exporttolibrarybtn").click(function() {
+                midipatch.exportToLibraryDialog(); 
+            });
+
+            $("#resetlayout").click(function() {
+                midipatch.resetLayout();
+            });
+
+            $("#clearallnotes").click(function() {
+                midipatch.clearAllNotes();
+            });
+
+            $("#editorselect").change(function() {
+                midipatch.setEditorMode($("#editorselect").val());
+            });
+        }
+}
