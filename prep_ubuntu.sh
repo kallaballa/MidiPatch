b96256491507174f6b3a422d17ea4adfc3c8b9b4
kallaballa <amir@viel-zu.org>
2019-11-23 20:12:15 +0100

    create a script to prepare an ubuntu system for midipatch

--- prep_ubuntu.sh
+++ prep_ubuntu.sh
@@ -9,5 +9,5 @@ apt-get update
 # install dependencies
 apt-get install git-core build-essential g++-7 zlib1g-dev libasound2-dev liblilv-dev npm libsqlite3-dev
 npm install -g inliner
-ln -s /usr/bin/nodejs /usr/bin/node
+ln -fs /usr/bin/nodejs /usr/bin/node
 
