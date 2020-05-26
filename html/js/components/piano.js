7a7515107dae0f938314a058d5cc96f0f1b91973
kallaballa <amir@viel-zu.org>
2020-05-26 23:16:28 +0200

    prevent sending of consecutive keydown events

--- html/js/components/piano.js
+++ html/js/components/piano.js
@@ -152,6 +152,7 @@ class MPPiano {
         var mp = this.midipatch;
         var all = $("*");
         var hold = $("#holdmode");
+        var activeKeys = [false, false, false, false, false, false, false, false, false, false, false, false];
 
         const keys = Array.from(document.querySelectorAll('.key'));
         keys.forEach(key => key.addEventListener('mousedown', function(e) {
@@ -183,34 +184,40 @@ class MPPiano {
         
         all.keydown(function(e) {
             var num = -1;
-            
+
             if(e.code.length < 2 || e.code.charAt(0) !== 'F' || isNaN(num = parseInt(e.code.substring(1))))
                 return;
-                       
-            var target = keys[ num - 1];
-
-            if (hold.prop("checked") && target.classList.has("playing")) {
-                e.preventDefault();
-                target.classList.remove('playing');
-                mp.noteOff(num - 1 + 36);
+            
+            e.preventDefault();
+                      
+            if(activeKeys[num - 1]) {
+                return;
             } else {
-                e.preventDefault();
-                target.classList.add('playing');
-                mp.noteOn(num - 1 + 36, 127);
+                activeKeys[num - 1] = true;
             }
+            
+            var target = keys[ num - 1];
+            target.classList.add('playing');
+            mp.noteOn(num - 1 + 36, 127);
         });
         all.keyup(function(e) {
             var num = -1;
              if(e.code.length < 2 || e.code.charAt(0) !== 'F' || isNaN(num = parseInt(e.code.substring(1))))
                 return;
+            e.preventDefault();
+            
+            if(activeKeys[num - 1]) {
+                activeKeys[num - 1] = false;
+            } else {
+                return;
+            }
             
             var target = keys[ num - 1];
-            
-            if (!hold.prop("checked")) {
-                e.preventDefault();
-                target.classList.remove('playing');
-                mp.noteOff(num - 1 + 36);
-            }
+            console.log(target);
+            e.preventDefault();
+            target.classList.remove('playing');
+            mp.noteOff(num - 1 + 36);
+        
         });
      
     }
